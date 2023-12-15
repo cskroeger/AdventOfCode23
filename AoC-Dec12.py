9583a71d60fdd6c0cc0a860d338f9e40cc7357f0
cskroeger <152728806+cskroeger@users.noreply.github.com>
2023-12-15 11:46:13 -0700

    Day 12: Hot Springs, part 2

--- AoC-Dec12.py
+++ AoC-Dec12.py
@@ -5,6 +5,7 @@
 # Shawn Kroeger
 #--------------------------------------------
 import re
+configs = {}
 
 # Hot Springs
 # Search Rules:
@@ -22,16 +23,32 @@ import re
 # ?#???#????????.?? 5,2,4,1
 # ???????????# 3,1,1,3
 def find_matches(strg, groups):
+    global configs
+    lst = '_'.join([str(i) for i in groups])
+    if (strg + lst) in configs:
+        #print("CONFIGS LOOKUP \'{}\' = {}".format(strg+lst, configs[strg + lst]))
+        return configs[strg + lst]
+    
     num_possibilities = 0
     n = groups[0]        # Length of current group.  We must have this many ? or # in a row
-    m = re.search(r"(^[#?]{%d})" %(n), strg[:n])  # Search for # or ? in the first n characters
+    if len(strg) >= n:
+        match = True
+        for i in range(n):
+            if strg[i] != '#' and strg[i] != '?':
+                match = False
+                break
+    else:
+        match = False
     
-    if m and (m.group(1) != None) and not (len(strg) > n and strg[n] == '#'):    # Found a match!
+    if match and not (len(strg) > n and strg[n] == '#'):    # Found a match!
         new_grp = groups[1::]   # Reduce groups by 1 and search for remaining solution
         if len(strg) > n+1 and new_grp != []:
-            num_possibilities += find_matches(strg[n+1:], new_grp)
+            fm = find_matches(strg[n+1:], new_grp)
+            configs[strg[n+1:] + '_'.join([str(i) for i in new_grp])] = fm
+            num_possibilities += fm
         elif (new_grp == [] and re.search(r"#", strg[n:]) == None):  # Ensure no #s were left behind
             num_possibilities += 1
+            configs[strg + '_' + str(groups[0])] = 1
     
     if len(strg) > 1 and strg[0] != '#':  # cannot bypass #s; they must be consumed
         num_possibilities += find_matches(strg[1:], groups)
@@ -39,7 +56,6 @@ def find_matches(strg, groups):
     return num_possibilities
 
 # Part 2
-# WARNING: The current implementation takes way too long; it's not yet viable
 def unfold_records(record, nums):
     record += ("?" + record) * 4
     nums *= 5
@@ -51,6 +67,7 @@ def parse_data(file, part2=False):
     total_arrangements = 0
     with open(file, "r") as f:
         for line in f:
+            configs.clear()
             record, nums = line.strip().split(" ")
             nums = list(nums.split(","))
             nums = [int(i) for i in nums]
@@ -59,11 +76,14 @@ def parse_data(file, part2=False):
             line_arrangements = find_matches(record, nums)
             total_arrangements += line_arrangements
             print(record, nums, "=> Arrangements:", line_arrangements)
+            if total_arrangements == 0:
+                print("ERROR: no arrangements found.  Exiting.")
+                quit()
     return total_arrangements
     print(record)
 
 
 if __name__ == "__main__":
-    print("Total Arrangements (part 1) =", parse_data("Aoc12_test.txt"))
-    print("Total Arrangements (part 2) =", parse_data("Aoc12_test.txt", True))
+    print("Total Arrangements (part 1) =", parse_data("Aoc12.txt"))
+    print("Total Arrangements (part 2) =", parse_data("Aoc12.txt", True))
     
